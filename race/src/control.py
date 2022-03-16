08ecdb8d1894eb97ee942f577a49e26be534b508
Simeng Hao <sh4aj@virginia.edu>
2022-03-15 22:09:27 -0400

    fix control node

--- race/src/control.py
+++ race/src/control.py
@@ -7,13 +7,17 @@ from ackermann_msgs.msg import AckermannDrive
 
 
 class Controller:
-	default_kp = 0.0
-	default_kd = 0.0
-	default_ki = 0.0
-	default_vel = 0.0
+	default_kp = 1.0
+	default_kd = 1.0
+	default_ki = 1.0
+	default_vel = 15.0
 	servo_offset = 0.0
+	active_frequency = 100
 
-	def __init__(self):
+	def __init__(self, active=False):
+		"""
+		@param active: whether to run in active mode, if in active mode, node publishes at fixed interval asynchronously with respect to receiving input, if in passive mode, node publishes each time input is received
+		"""
 		# error memory
 		self.error_memory_size = 10
 		self.error_memory = deque()
@@ -22,10 +26,16 @@ class Controller:
 		self.kp = Controller.default_kp
 		self.kd = Controller.default_kd
 		self.ki = Controller.default_ki
+		self.vel = Controller.default_vel
+		self.angle = 0.0
 
-		# Active frequency
-		self.frequency = 100
-		self.interval = 1/self.frequency
+		# set mode
+		self.active = active
+		
+		if self.active:
+			# Active frequency
+			self.frequency = Controller.active_frequency
+			self.interval = 1/self.frequency
 
 		# This code can input desired velocity from the user.
 		# velocity must be between [0,100] to move forward. 
@@ -36,54 +46,54 @@ class Controller:
 		# > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
 		self.vel_input = Controller.default_vel
 
-		rospy.init_node('pid_controller', anonymous=True)
+		# node config
+		rospy.init_node('pid_controller', anonymous=False)
+		rospy.on_shutdown(self.shutdown)
 
 
-
-		rospy.Subscriber("error", pid_input, self.register_pid_input)
+		rospy.Subscriber("error", pid_input, self.error_listener_hook)
 
 		# Publisher for moving the car. 
-		# TODO: Use the coorect topic /car_x/offboard/command.
-		self.command_pub = rospy.Publisher('/car_1/offboard/command', AckermannDrive, queue_size = 1)
+		self.command_pub = rospy.Publisher('/car_1/offboard/command', AckermannDrive, queue_size = 10)
+
+		if active:
+			self.run_active()
 
 	def generate_control_message(self):
-
-		angle = 0.0
-
-		print("PID Control Node is Listening to error")
-		
-		## Your PID code goes here
-		#TODO: Use kp, ki & kd to implement a PID controller
-		
-		# 1. Scale the error
-		# 2. Apply the PID equation on error to compute steering
-		
 		# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
 		command = AckermannDrive()
 
-		# TODO: Make sure the steering value is within bounds [-100,100]
-		command.steering_angle = angle
+		# print("PID Control Node is Listening to error")
+		
+		## Your PID code goes here
+		
+		# 1. Scale the error
+		# 2. Apply the PID equation on error to compute steering
 
-		# TODO: Make sure the velocity is within bounds [0,100]
+		p,i,d = self.get_pid()
+		pid_output = self.kp * p + self.ki * i + self.kd * d
+		# print("pid_output: ",pid_output)
+		
+		# convert pid_output_to steering angle through tanh function, *100 since range is [-100,100]
+		self.angle = math.tanh(pid_output)*100
+
+		command.steering_angle = self.angle
 		command.speed = self.vel_input
 
-		# Move the car autonomously
-		# command_pub.publish(command)
 		return command
 
 	def register_pid_input(self, pid_input):
-		velocity, error = pid_input
-		if len(self.error_memory) < self.error_memory_size:
-			self.error_memory.append(error)
-		else:
-			self.error_memory.append(error)
+		# return
+		error = pid_input.pid_error
+		self.error_memory.append(error)
+		if len(self.error_memory) >= self.error_memory_size:
 			self.error_memory.popleft()
 
 	def set_gains(self,kp, kd, ki, vel_input):
 		self.kp = kp
 		self.kd = kd
 		self.ki = ki
-		self.vel_input = vel_input
+		self.vel = vel_input
 		
 	def get_pid(self):
 		if len(self.error_memory) == 0:
@@ -91,22 +101,38 @@ class Controller:
 		if len(self.error_memory) == 1:
 			return self.error_memory[0], 0, 0
 		p = self.error_memory[-1]
-		i = math.sum(self.error_memory)
+		i = sum(self.error_memory)
 		d = self.error_memory[-1] - self.error_memory[-2]
 		return p,i,d
 
-	def run(self):
-		while True:
+	def run_active(self):
+		while not rospy.is_shutdown():
 			control_msg = self.generate_control_message()
 			self.command_pub.publish(control_msg)
 			rospy.sleep(self.interval)
 
-if __name__ == '__main__':
-	kp = int(input("Enter Kp Value: "))
-	kd = int(input("Enter Kd Value: "))
-	ki = int(input("Enter Ki Value: "))
-	vel_input = int(input("Enter desired velocity: "))
+	def error_listener_hook(self, pid_input):
+		self.register_pid_input(pid_input)
 
-	controller = Controller()
-	controller.set_gains(kp, kd, ki, vel_input)
-	controller.run()
+		# If in passive mode: need listener to trigger publication
+		if not self.active:
+			control_msg = self.generate_control_message()
+			self.command_pub.publish(control_msg)
+
+	def shutdown(self):
+		rospy.sleep(0.1)
+		print("Shutting down")
+
+
+if __name__ == '__main__':
+	# kp = int(input("Enter Kp Value: "))	
+	# kd = int(input("Enter Kd Value: "))
+	# ki = int(input("Enter Ki Value: "))
+	# vel_input = int(input("Enter desired velocity: "))
+	try:
+		controller = Controller(active=False)
+	except:
+		rospy.signal_shutdown()
+	
+	rospy.spin()
+	# controller.set_gains(kp, kd, ki, vel_input)
