ddf376c48dd5d4643c212e30932c40dceec18f8a
Simeng Hao <sh4aj@virginia.edu>
2022-04-06 12:27:28 -0400

    add some empty functions

--- race/src/ftg.py
+++ race/src/ftg.py
@@ -9,8 +9,19 @@ from std_msgs.msg import Float32MultiArray
 class FTGController:
 
     def __init__(self):
+        rospy.init_node('ftg_controller', anonymous=False)
+        rospy.on_shutdown(self.shutdown)
+        self.command_pub = rospy.Publisher('/car_1/offboard/command', AckermannDrive, queue_size = 10)
+        rospy.Subscriber("/car_1/scan", pid_input, self.scan_listener_hook)
+    
+    def scan_listener_hook(self):
         pass
-    pass
+
+    def generate_control_message(self):
+        pass
+
+    def disparity_extend(self, scan):
+        pass
 
 if __name__ == '__main__':
     try:
