b223a2412a6f8114657c6a9e328663c6dbce7038
Simeng Hao <sh4aj@virginia.edu>
2022-04-06 12:18:24 -0400

    add main method

--- race/src/ftg.py
+++ race/src/ftg.py
@@ -1,2 +1,19 @@
+#!/usr/bin/env python
+from collections import deque
+import math
+import rospy
+from race.msg import pid_input
+from ackermann_msgs.msg import AckermannDrive
+from std_msgs.msg import Float32MultiArray
+
 class FTGController:
+
+    def __init__(self):
+        pass
     pass
+
+if __name__ == '__main__':
+    try:
+        controller = FTGController(active=False, tune=True)
+    except:
+        rospy.signal_shutdown()
