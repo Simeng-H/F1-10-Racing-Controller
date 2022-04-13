86fc627526ce3a453feac6c0fc5cf1c7e3f2d2ab
Simeng Hao <sh4aj@virginia.edu>
2022-04-13 12:00:35 -0400

    add index to angle method

--- race/src/ftg.py
+++ race/src/ftg.py
@@ -24,8 +24,8 @@ class FTGController:
     
     def scan_listener_hook(self, laser_scan):
         self.preprocess_and_save_scan(laser_scan)
-        self.disparity_extend(self.ranges)
-        # self.generate_and_publish_control_message()
+        angle = self.disparity_extend(self.ranges)
+        self.generate_and_publish_control_message(angle, 4)
         pass
 
     def preprocess_and_save_scan(self, laser_scan):
@@ -35,11 +35,19 @@ class FTGController:
         self.raw_scan = laser_scan
         self.angle_increment = laser_scan.angle_increment
         ranges = [raw if raw != float('nan') else laser_scan.range_max for raw in laser_scan.ranges]
+        for index in range(len(ranges)):
+            if ranges[index] < 0.05:
+                ranges[index] = 0.05
         self.ranges = ranges
 
     def angle_to_index(self, angle):
         index = int(float(angle-self.raw_scan.angle_min)/self.raw_scan.angle_increment)
         return index
+
+    def index_to_angle(self, index):
+        # print((self.raw_scan.angle_max - self.raw_scan.angle_min) * 180 / math.pi)
+        angle = index*1.0/len(self.ranges) * (self.raw_scan.angle_max - self.raw_scan.angle_min) + self.raw_scan.angle_min
+        return angle
 
     def get_frontal_clearance(self):
         start = self.angle_to_index(-math.pi/10)
@@ -49,13 +57,18 @@ class FTGController:
     def generate_and_publish_control_message(self, target_angle, target_distance):
         angle = self.generate_steering_angle(target_angle)
         speed = self.generate_speed(target_distance)
-        command = self.make_control_message(angle, speed)
+        command = self.make_control_message(angle, 17)
         self.command_pub.publish(command)
 
     def generate_steering_angle(self, target_angle):
         angle = target_angle/FTGController.max_steering_angle * 100
         if angle >= 100:
             angle = 100
+        if angle <= -100:
+            angle = -100
+        if min(self.ranges) < FTGController.car_radius:
+            print("minimum range: %f" % min(self.ranges))
+            angle = 0
         return angle
         
     def generate_speed(self, target_distance):
@@ -78,35 +91,43 @@ class FTGController:
         num = len(ranges)
         start_range = int(0.125*num) # ignore the first 30 degrees
         end_range = int(0.875*num) # ignore the last 30 degrees
-        prev = ranges[start_range]
-        k = start_range+1
-        while k < end_range:
-            curr = ranges[k]
-            if curr - prev > 1:
-                angle = (FTGController.car_radius/prev)
-                num_rays = int(angle/self.raw_scan.angle_increment)
-                for i in range(k, k+num_rays):
-                    ranges[i] = prev
-                    k += 1
-            elif prev - curr < 1:
-                angle = (FTGController.car_radius/curr)
-                num_rays = int(angle/self.raw_scan.angle_increment)
-                for i in range(k-num_rays, k):
-                    # print(ranges[i])
-                    # print(curr)
-                    ranges[i] = curr
-            prev = curr
-            k += 1
+        # prev = ranges[start_range]
+        # k = start_range+1
+        # while k < end_range:
+        #     try:
+        #         curr = ranges[k]
+        #         if curr - prev > 1:
+        #             angle = (FTGController.car_radius/prev)
+        #             num_rays = int(angle/self.raw_scan.angle_increment)
+        #             for i in range(k, k+num_rays):
+        #                 # print(angle, num_rays ,i, len(ranges))
+        #                 ranges[i] = prev
+        #                 k += 1
+        #         elif prev - curr < 1:
+        #             angle = (FTGController.car_radius/curr)
+        #             num_rays = int(angle/self.raw_scan.angle_increment)
+        #             for i in range(k-num_rays, k):
+        #                 # print(ranges[i])
+        #                 # print(curr)
+        #                 # print(i, len(ranges))
+        #                 ranges[i] = curr
+        #         prev = curr
+        #         k += 1
+        #     except IndexError:
+        #         k += 1
 
         best_ray = start_range
         best_distance = 0
         for k in range(start_range+1, end_range):
-            if ranges[k] > best_distance:
+            if ranges[k] > best_distance+0.1:
                 best_distance = ranges[k]
                 best_ray = k
 
-        print("best angle: %f" % (best_ray/num)*240 - 120)
-        return (best_ray/num)*240 - 120
+        # print("best ray: %f" % best_ray)
+        # print("best ray: %f" % (float(best_ray)/num))
+        best_angle = self.index_to_angle(best_ray) * 180/math.pi
+        print("best angle: %f" % best_angle)
+        return best_angle
 
 if __name__ == '__main__':
     controller = FTGController()
