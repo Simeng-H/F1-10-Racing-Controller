32f232f0549dc038a6af690ef2b431b47146ea71
Denis <drcall4@outlook.com>
2022-04-16 13:49:30 -0400

    Modified network.sh

--- network.sh
+++ network.sh
@@ -8,12 +8,8 @@ CASE=$1
 PROFILE=f1tenth # Name of your f1tenth network config profile
 
 if [ "${CASE}" = "enable" ]; then
-  export ROS_MASTER_URI=http://192.168.1.1:11311
-  export ROS_IP=192.168.1.105
   nmcli c up ${PROFILE}
 elif [ "${CASE}" = "disable" ]; then
-  unset ROS_MASTER_URI
-  unset ROS_IP
   nmcli c down ${PROFILE}
 else
   echo "Usage: source network.sh [enable/disable] INS";
